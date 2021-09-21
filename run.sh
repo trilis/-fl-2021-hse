bad0d641ec674a0ca605a1e6e003378e3d1e9c66
naumtsev <31536156+naumtsev@users.noreply.github.com>
2021-09-21 17:17:12 +0300

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 flex -o ./build/lex.yy.c ./build/lexer.l
-gcc ./build/lex.yy.c -o lexer
+gcc ./build/lex.yy.c -o ./build/lexer
 
 ls ./tests/ -1 | while read file 
 do
-    ./lexer <./tests/$file >./testsout/$file.out
+    ./build/lexer <./tests/$file >./testsout/$file.out
 done
