25fefd8ccc0e04418d77b537ae26945756ef9b89
Vyacheslav Shabes <71638897+VTGcon@users.noreply.github.com>
2021-09-25 23:47:32 +0300

    Fixed 4 task

--- lexer.cpp
+++ lexer.cpp
@@ -7,20 +7,15 @@ int id = 1;
 %option yylineno
 %option noyywrap
 
-T_NUM_OF_VERTEX [0-9]+
-T_VERTEX_TYPE [0-9]+,[01][01][01][01]
-T_VERTEX_FUNC [0-9]+(,[0-9]+)*
-
+T_NUMBER [0-9]+
 
 %%
 
 [!]         {printf("Exclamation mark %u %d\n", yylineno, id); id++;}
 [@]         {printf("At sign in line %u %d\n", yylineno, id); id++;}
 [;]         {printf("Semicolon at %u %d\n", yylineno, id); id++;}
-{T_NUM_OF_VERTEX} {printf("Num of vertex %s %u %d\n", yytext, yylineno, id); id += strlen(yytext);}
-{T_VERTEX_TYPE} {printf("Type of edge %s %u %d\n", yytext, yylineno, id); id += strlen(yytext);}
-{T_VERTEX_FUNC} {printf("Edge function %s %u %d\n", yytext, yylineno, id); id += strlen(yytext);}
-
+[,]         {printf("Comma at %u %d\n", yylineno, id); id++;}
+{T_NUMBER} {printf("Number %s %u %d\n", yytext, yylineno, id); id += strlen(yytext);}
 
 . {
 printf("Error at id %d\n", id);
