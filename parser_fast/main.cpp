b24536cde1e6489b5f6b608d2df3260c7c1d45aa
Sergey Morozov <morozov.serg901@gmail.com>
2021-09-28 12:04:35 +0300

    Added more regular expressions

--- parser_fast/main.cpp
+++ parser_fast/main.cpp
@@ -8,10 +8,10 @@ int main(int argc, char *argv[]) {
   }
 
   std::shared_ptr<Regex> r1(new Concat(new Char('a'), new Char('b')));
-  std::shared_ptr<Regex> r2(new Star(new Concat(new Char('a'), new Char('b'))));
+  std::shared_ptr<Regex> r2(new Star(new Char('a')));
   std::shared_ptr<Regex> r3(new Star(new Epsilon()));
-  std::shared_ptr<Regex> r4(new Star(new Concat(new Char('a'), new Char('b'))));
-  std::shared_ptr<Regex> r5(new Star(new Concat(new Char('a'), new Char('b'))));
+  std::shared_ptr<Regex> r4(new Concat(new Star(new Concat(new Char('a'), new Char('b'))), new Alt(new Star(new Concat(new Char('c'), new Char('d'))), new Star(new Concat(new Char('e'), new Char('f'))))));
+  std::shared_ptr<Regex> r5(new Star(new Alt(new Concat(new Char('0'), new Char('0')), new Concat(new Char('1'), new Char('1')))));
 
   std::shared_ptr<Regex> samples[] = {r1, r2, r3, r4, r5};
   assert(sizeof(samples) > 0);
