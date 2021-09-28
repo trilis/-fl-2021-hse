0afbd8b1b8c037625b44b2bb407b39233b0ce2c6
Egor Sheremetov <egor.sheremetov@yandex.ru>
2021-09-28 05:40:50 +0300

    Added optimizations

--- regex_engine.cpp
+++ regex_engine.cpp
@@ -11,144 +11,228 @@
 
 using namespace regex_engine;
 
-std::shared_ptr<abstract_regex>
-regex_engine::intersect(std::shared_ptr<abstract_regex> const &lhs, std::shared_ptr<abstract_regex> const &rhs) {
-    if (lhs->is_epsilon() && rhs->is_epsilon()) {
-        return std::make_shared<epsilon_regex>();
+common_regex_t regex_engine::operator+(common_regex_t const &lhs, common_regex_t const &rhs) {
+  return concat(lhs, rhs);
+}
+
+common_regex_t regex_engine::operator*(common_regex_t const &lhs) {
+  return many(lhs);
+}
+
+common_regex_t regex_engine::operator|(common_regex_t const &lhs, common_regex_t const &rhs) {
+  return either(lhs, rhs);
+}
+
+// WARNING: shit code
+bool regex_engine::are_equal(common_regex_t const &lhs, common_regex_t const &rhs) {
+  if (lhs->type() != rhs->type()) return false;
+
+  // Can be done faster I guess, works in O(#if * dynamic_cast)
+
+  if (lhs->type() == CHAR) {
+    return std::dynamic_pointer_cast<char_regex>(lhs)->value == std::dynamic_pointer_cast<char_regex>(rhs)->value;
+  }
+
+  if (lhs->type() == STAR) {
+    return are_equal(std::dynamic_pointer_cast<star_regex>(lhs)->regex,
+                     std::dynamic_pointer_cast<star_regex>(rhs)->regex);
+  }
+
+  if (lhs->type() == ALT) {
+    std::shared_ptr<alt_regex> lhs_cast = std::dynamic_pointer_cast<alt_regex>(lhs);
+    std::shared_ptr<alt_regex> rhs_cast = std::dynamic_pointer_cast<alt_regex>(rhs);
+    return are_equal(lhs_cast->alt1, rhs_cast->alt1) && are_equal(lhs_cast->alt2, rhs_cast->alt2);
+
+  }
+
+  if (lhs->type() == CONCAT) {
+    std::shared_ptr<concat_regex> lhs_cast = std::dynamic_pointer_cast<concat_regex>(lhs);
+    std::shared_ptr<concat_regex> rhs_cast = std::dynamic_pointer_cast<concat_regex>(rhs);
+    return are_equal(lhs_cast->concat1, rhs_cast->concat1) && are_equal(lhs_cast->concat2, rhs_cast->concat2);
+  }
+
+  // I could have tried to do same kind of effect by defining a virtual function, that would work, but then
+  // I would have dynamic_cast in my code anyway or maybe use some visitor pattern, that is less disgusting, but still pretty messy.
+  // I will try to think of a better solution though
+
+
+  return true;
+
+}
+
+bool regex_engine::is_epsilon(common_regex_t const &regex) {
+  return regex->type() == EPSILON;
+}
+
+bool regex_engine::is_empty(common_regex_t const &regex) {
+  return regex->type() == EMPTY;
+}
+
+common_regex_t
+regex_engine::concat(common_regex_t const &lhs, common_regex_t const &rhs) {
+  if (is_empty(lhs) || is_empty(rhs)) {
+    return empty_set();
+  }
+  if (is_epsilon(lhs) || is_epsilon(rhs)) {
+    return is_epsilon(lhs) ? rhs : lhs;
+  }
+  return std::make_shared<concat_regex>(lhs, rhs);
+}
+
+common_regex_t
+regex_engine::either(common_regex_t const &lhs, common_regex_t const &rhs) {
+  if (is_empty(lhs) || is_empty(rhs)) {
+    return is_empty(lhs) ? rhs : lhs;
+  }
+  if (is_epsilon(lhs) || is_epsilon(rhs)) {
+    if (is_epsilon(lhs) && rhs->nullable()) {
+      return rhs;
     }
-    return std::make_shared<empty_regex>();
-}
-
-std::shared_ptr<abstract_regex>
-regex_engine::unite(std::shared_ptr<abstract_regex> const &lhs, std::shared_ptr<abstract_regex> const &rhs) {
-    if (lhs->is_epsilon() || rhs->is_epsilon()) {
-        return std::make_shared<epsilon_regex>();
+    if (is_epsilon(rhs) && lhs->nullable()) {
+      return lhs;
     }
-    return std::make_shared<empty_regex>();
+  }
+  if (are_equal(lhs, rhs)) {
+    return lhs;
+  }
+  return std::make_shared<alt_regex>(lhs, rhs);
 }
 
-std::shared_ptr<abstract_regex>
-regex_engine::concat(std::shared_ptr<abstract_regex> const &lhs, std::shared_ptr<abstract_regex> const &rhs) {
-    return std::make_shared<concat_regex>(lhs, rhs);
+common_regex_t regex_engine::many(common_regex_t const &regex) {
+  if (is_empty(regex) || is_epsilon(regex)) {
+    return is_empty(regex) ? empty_set() : epsilon();
+  }
+  if (regex->type() == STAR) {
+    return regex;
+  }
+  return std::make_shared<star_regex>(regex);
 }
 
-std::shared_ptr<abstract_regex>
-regex_engine::either(std::shared_ptr<abstract_regex> const &lhs, std::shared_ptr<abstract_regex> const &rhs) {
-    return std::make_shared<alt_regex>(lhs, rhs);
-}
+common_regex_t regex_engine::symb(char c) { return std::make_shared<char_regex>(c); }
 
-std::shared_ptr<abstract_regex> regex_engine::many(std::shared_ptr<abstract_regex> const &regex) {
-    return std::make_shared<star_regex>(regex);
-}
+common_regex_t regex_engine::empty_set() { return std::make_shared<empty_regex>(); }
 
-std::shared_ptr<abstract_regex> regex_engine::symb(char c) {
-    return std::make_shared<char_regex>(c);
-}
-
-std::shared_ptr<abstract_regex> regex_engine::empty_set() {
-    return std::make_shared<empty_regex>();
-}
-
-std::shared_ptr<abstract_regex> regex_engine::epsilon() {
-    return std::make_shared<epsilon_regex>();
-}
+common_regex_t regex_engine::epsilon() { return std::make_shared<epsilon_regex>(); }
 
 /* EMPTY REGEX, begin */
-std::shared_ptr<abstract_regex> empty_regex::nullable() {
-    return empty_set();
-}
+bool empty_regex::nullable() { return false; }
 
-std::shared_ptr<abstract_regex> empty_regex::derivative(char c) {
-    return empty_set();
-}
+common_regex_t empty_regex::derivative(char c) { return empty_set(); }
 
-bool empty_regex::is_empty() const { return true; }
+regex_types empty_regex::type() const { return EMPTY; }
 /* EMPTY REGEX, end */
 
 /* EPSILON REGEX, begin */
-std::shared_ptr<abstract_regex> epsilon_regex::nullable() {
-    return epsilon();
-}
+bool epsilon_regex::nullable() { return true; }
 
-std::shared_ptr<abstract_regex> epsilon_regex::derivative(char c) {
-    return empty_set();
-}
+common_regex_t epsilon_regex::derivative(char c) { return empty_set(); }
 
-bool epsilon_regex::is_epsilon() const { return true; }
+regex_types epsilon_regex::type() const { return EPSILON; }
 /* EPSILON REGEX, end */
 
 /* CONCAT REGEX, begin */
-concat_regex::concat_regex(std::shared_ptr<abstract_regex> concat1, std::shared_ptr<abstract_regex> concat2)
-    : concat1(std::move(concat1)),
-    concat2(std::move(concat2)) {}
+concat_regex::concat_regex(common_regex_t concat1, common_regex_t concat2)
+        : concat1(std::move(concat1)),
+          concat2(std::move(concat2)) {}
 
-std::shared_ptr<abstract_regex> concat_regex::nullable() {
-    return intersect(concat1->nullable(), concat2->nullable());
+bool concat_regex::nullable() { return concat1->nullable() && concat2->nullable(); }
+
+
+common_regex_t concat_regex::derivative(char c) {
+  return either(
+          concat(concat1->derivative(c), concat2),
+          !concat1->nullable() ? empty_set() : concat2->derivative(c)
+  );
 }
 
-
-std::shared_ptr<abstract_regex> concat_regex::derivative(char c) {
-    return either(
-            concat(concat1->derivative(c), concat2),
-            concat(concat1->nullable(), concat2->derivative(c))
-            );
-}
+regex_types concat_regex::type() const { return CONCAT; }
 /* CONCAT REGEX, end */
 
 /* STAR REGEX, begin */
-star_regex::star_regex(std::shared_ptr<abstract_regex> regex) : regex(std::move(regex)) {}
+star_regex::star_regex(common_regex_t regex) : regex(std::move(regex)) {}
 
-std::shared_ptr<abstract_regex> star_regex::nullable() {
-    return epsilon();
-}
+bool star_regex::nullable() { return true; }
 
-std::shared_ptr<abstract_regex> star_regex::derivative(char c)  {
-    return concat(regex->derivative(c), many(regex));
-}
+common_regex_t star_regex::derivative(char c) { return concat(regex->derivative(c), many(regex)); }
+
+regex_types star_regex::type() const { return STAR; }
 /* STAR REGEX, end */
 
 /* ALT REGEX, begin */
-alt_regex::alt_regex(std::shared_ptr<abstract_regex> alt1, std::shared_ptr<abstract_regex> alt2)
-    : alt1(std::move(alt1)),
-    alt2(std::move(alt2)) {}
+alt_regex::alt_regex(common_regex_t alt1, common_regex_t alt2)
+        : alt1(std::move(alt1)),
+          alt2(std::move(alt2)) {}
 
-std::shared_ptr<abstract_regex> alt_regex::nullable() {
-    return unite(alt1->nullable(), alt2->nullable());
+bool alt_regex::nullable() { return alt1->nullable() || alt2->nullable(); }
+
+common_regex_t alt_regex::derivative(char c) {
+  auto lhs = alt1->derivative(c);
+  auto rhs = alt2->derivative(c);
+  return either(lhs, rhs);
 }
 
-std::shared_ptr<abstract_regex> alt_regex::derivative(char c) {
-    return either(alt1->derivative(c), alt2->derivative(c));
-}
+regex_types alt_regex::type() const { return ALT; }
 /* ALT REGEX, end */
 
 /* CHAR REGEX, begin */
 char_regex::char_regex(char c) : value(c) {}
 
-std::shared_ptr<abstract_regex> char_regex::nullable() {
-    return empty_set();
+bool char_regex::nullable() { return false; }
+
+common_regex_t char_regex::derivative(char c) {
+  if (value == c) {
+    return epsilon();
+  }
+  return empty_set();
 }
 
-std::shared_ptr<abstract_regex> char_regex::derivative(char c) {
-    if (value == c) {
-        return epsilon();
-    }
-    return empty_set();
-}
+regex_types char_regex::type() const { return CHAR; }
+
 /* CHAR REGEX, end */
 
+
+
+common_regex_t derivative_strike(std::string &pattern, common_regex_t regex) {
+  if (pattern.empty()) {
+    return regex;
+  }
+  char front_char = pattern.back();
+  pattern.pop_back();
+  return derivative_strike(pattern, regex->derivative(front_char));
+}
+
+bool match_internal(std::string &pattern, common_regex_t regex) {
+  return derivative_strike(pattern, std::move(regex))->nullable();
+}
+
+bool match(std::string pattern, common_regex_t regex) {
+  std::reverse(std::begin(pattern), std::end(pattern));
+  return match_internal(pattern, std::move(regex));
+}
+
 void test() {
-    assert(match("aabbba", concat(symb('a'), many(either(symb('a'), symb('b'))))));
-    assert(!match("caaa", many(either(symb('a'), symb('b')))));
-    assert(match("caaa", many(either(symb('a'), symb('c')))));
-    assert(match("aaaabbbbb", concat(many(symb('a')), many(symb('b')))));
-    assert(!match("bbbbaaaaa", concat(many(symb('a')), many(symb('b')))));
-    assert(match("ababab", many(either(concat(symb('a'), symb('b')), concat(symb('b'), symb('a'))))));
-    assert(!match("ababa", many(either(concat(symb('a'), symb('b')), concat(symb('b'), symb('a'))))));
-    assert(match("bababa", many(either(concat(symb('a'), symb('b')), concat(symb('b'), symb('a'))))));
-    assert(!match("babab", many(either(concat(symb('a'), symb('b')), concat(symb('b'), symb('a'))))));
+  assert(match("aabbba", concat(symb('a'), many(either(symb('a'), symb('b'))))));
+  assert(!match("caaa", many(either(symb('a'), symb('b')))));
+  assert(match("caaa", many(either(symb('a'), symb('c')))));
+  assert(match("aaaabbbbb", concat(many(symb('a')), many(symb('b')))));
+  assert(!match("bbbbaaaaa", concat(many(symb('a')), many(symb('b')))));
+  assert(match("ababab", many(either(concat(symb('a'), symb('b')), concat(symb('b'), symb('a'))))));
+  assert(!match("ababa", many(either(concat(symb('a'), symb('b')), concat(symb('b'), symb('a'))))));
+  assert(match("bababa", many(either(concat(symb('a'), symb('b')), concat(symb('b'), symb('a'))))));
+  assert(!match("babab", many(either(concat(symb('a'), symb('b')), concat(symb('b'), symb('a'))))));
+  // Doesn't even finish without current optimizations
+  assert(match("aaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb", many(either(symb('a'), symb('b')))));
+  // Equivalent to first assert
+  assert(match("aabbba", symb('a') + *(symb('a') | symb('b'))));
+  assert(are_equal(symb('a'), symb('a')));
+  assert(!are_equal(symb('a'), symb('b')));
+  assert(are_equal(symb('a') | symb('c'), symb('a') | symb('c')));
+  assert(!are_equal(symb('a') | symb('c'), symb('a') | symb('b')));
+  assert(!are_equal(symb('a') | symb('c'), *(symb('a') | symb('b'))));
+  assert(!are_equal(*(symb('a') | symb('c')), *(symb('a') | symb('b'))));
 }
 
 int main() {
-    test();
-    return 0;
+  test();
+  return 0;
 }
--- regex_engine.h
+++ regex_engine.h
@@ -7,120 +7,125 @@
 
 namespace regex_engine {
 
-    struct empty_label_t {
-    };
-    struct epsilon_label_t {
-    };
+struct empty_label_t {
+};
+struct epsilon_label_t {
+};
 
-    struct abstract_regex {
-        virtual std::shared_ptr<abstract_regex> nullable() = 0;
+enum regex_types {
+  STAR, CHAR, ALT, CONCAT, EPSILON, EMPTY
+};
 
-        virtual std::shared_ptr<abstract_regex> derivative(char c) = 0;
+struct abstract_regex {
 
-        virtual bool is_empty() const { return false; }
+  virtual bool nullable() = 0;
 
-        virtual bool is_epsilon() const { return false; }
-    };
+  virtual std::shared_ptr<abstract_regex> derivative(char c) = 0;
 
-    struct empty_regex : abstract_regex {
-        // In case of SFINAE workarounds
-        empty_label_t empty_label;
+  virtual regex_types type() const = 0;
 
-        std::shared_ptr<abstract_regex> nullable() override;
+};
 
-        std::shared_ptr<abstract_regex> derivative(char c) override;
+using common_regex_t = std::shared_ptr<abstract_regex>;
 
-        bool is_empty() const override;
-    };
+bool is_epsilon(common_regex_t const &regex);
 
-    struct epsilon_regex : abstract_regex {
-        // In case of SFINAE workarounds
-        epsilon_label_t epsilon_label;
+bool is_empty(common_regex_t const &regex);
 
-        std::shared_ptr<abstract_regex> nullable() override;
+struct empty_regex : abstract_regex {
+  // In case of SFINAE workarounds, also occurred pretty handy in debugging,
+  // it was easier to see what type does object actually have
+  empty_label_t empty_label;
 
-        std::shared_ptr<abstract_regex> derivative(char c) override;
+  bool nullable() override;
 
-        bool is_epsilon() const override;
-    };
+  std::shared_ptr<abstract_regex> derivative(char c) override;
 
-    std::shared_ptr<abstract_regex>
-    intersect(std::shared_ptr<abstract_regex> const &lhs, std::shared_ptr<abstract_regex> const &rhs);
+  regex_types type() const override;
 
-    std::shared_ptr<abstract_regex>
-    unite(std::shared_ptr<abstract_regex> const &lhs, std::shared_ptr<abstract_regex> const &rhs);
+};
 
-    struct concat_regex : abstract_regex {
-        std::shared_ptr<abstract_regex> concat1, concat2;
+struct epsilon_regex : abstract_regex {
+  // In case of SFINAE workarounds
+  epsilon_label_t epsilon_label;
 
-        concat_regex(std::shared_ptr<abstract_regex> concat1, std::shared_ptr<abstract_regex> concat2);
+  bool nullable() override;
 
-        std::shared_ptr<abstract_regex> nullable() override;
+  std::shared_ptr<abstract_regex> derivative(char c) override;
 
-        std::shared_ptr<abstract_regex> derivative(char c) override;
-    };
+  regex_types type() const override;
 
-    struct star_regex : abstract_regex {
-        std::shared_ptr<abstract_regex> regex;
+};
 
-        explicit star_regex(std::shared_ptr<abstract_regex> regex);
+struct concat_regex : abstract_regex {
+  std::shared_ptr<abstract_regex> concat1, concat2;
 
-        std::shared_ptr<abstract_regex> nullable() override;
+  concat_regex(std::shared_ptr<abstract_regex> concat1, std::shared_ptr<abstract_regex> concat2);
 
-        std::shared_ptr<abstract_regex> derivative(char c) override;
-    };
+  bool nullable() override;
 
-    struct alt_regex : abstract_regex {
-        std::shared_ptr<abstract_regex> alt1, alt2;
+  std::shared_ptr<abstract_regex> derivative(char c) override;
 
-        alt_regex(std::shared_ptr<abstract_regex> alt1, std::shared_ptr<abstract_regex> alt2);
+  regex_types type() const override;
+};
 
-        std::shared_ptr<abstract_regex> nullable() override;
+struct star_regex : abstract_regex {
+  std::shared_ptr<abstract_regex> regex;
 
-        std::shared_ptr<abstract_regex> derivative(char c) override;
-    };
+  explicit star_regex(std::shared_ptr<abstract_regex> regex);
 
-    struct char_regex : abstract_regex {
-        char value = '\0';
+  bool nullable() override;
 
-        explicit char_regex(char c);
+  std::shared_ptr<abstract_regex> derivative(char c) override;
 
-        std::shared_ptr<abstract_regex> nullable() override;
+  regex_types type() const override;
+};
 
-        std::shared_ptr<abstract_regex> derivative(char c) override;
-    };
+struct alt_regex : abstract_regex {
+  std::shared_ptr<abstract_regex> alt1, alt2;
 
-    std::shared_ptr<abstract_regex> derivative_strike(std::string &pattern, std::shared_ptr<abstract_regex> regex) {
-        if (pattern.empty()) {
-            return regex;
-        }
-        char front_char = pattern.back();
-        pattern.pop_back();
-        return derivative_strike(pattern, regex->derivative(front_char));
-    }
+  alt_regex(std::shared_ptr<abstract_regex> alt1, std::shared_ptr<abstract_regex> alt2);
 
-    std::shared_ptr<abstract_regex> match_internal(std::string &pattern, std::shared_ptr<abstract_regex> regex) {
-        return derivative_strike(pattern, std::move(regex))->nullable();
-    }
+  bool nullable() override;
 
-    bool match(std::string pattern, std::shared_ptr<abstract_regex> regex) {
-        std::reverse(std::begin(pattern), std::end(pattern));
-        return match_internal(pattern, std::move(regex))->is_epsilon();
-    }
+  std::shared_ptr<abstract_regex> derivative(char c) override;
 
-    std::shared_ptr<abstract_regex>
-    concat(std::shared_ptr<abstract_regex> const &lhs, std::shared_ptr<abstract_regex> const &rhs);
+  regex_types type() const override;
+};
 
-    std::shared_ptr<abstract_regex>
-    either(std::shared_ptr<abstract_regex> const &lhs, std::shared_ptr<abstract_regex> const &rhs);
+struct char_regex : abstract_regex {
+  char value = '\0';
 
-    std::shared_ptr<abstract_regex> many(std::shared_ptr<abstract_regex> const &regex);
+  explicit char_regex(char c);
 
-    std::shared_ptr<abstract_regex> symb(char c);
+  bool nullable() override;
 
-    std::shared_ptr<abstract_regex> empty_set();
+  std::shared_ptr<abstract_regex> derivative(char c) override;
 
-    std::shared_ptr<abstract_regex> epsilon();
+  regex_types type() const override;
+};
+
+std::shared_ptr<abstract_regex>
+concat(std::shared_ptr<abstract_regex> const &lhs, std::shared_ptr<abstract_regex> const &rhs);
+
+std::shared_ptr<abstract_regex>
+either(std::shared_ptr<abstract_regex> const &lhs, std::shared_ptr<abstract_regex> const &rhs);
+
+std::shared_ptr<abstract_regex> many(std::shared_ptr<abstract_regex> const &regex);
+
+std::shared_ptr<abstract_regex> symb(char c);
+
+std::shared_ptr<abstract_regex> empty_set();
+
+std::shared_ptr<abstract_regex> epsilon();
+
+common_regex_t operator+(common_regex_t const &lhs, common_regex_t const &rhs);
+
+common_regex_t operator*(common_regex_t const &lhs);
+
+common_regex_t operator|(common_regex_t const &lhs, common_regex_t const &rhs);
+
+bool are_equal(common_regex_t const &lhs, common_regex_t const &rhs);
 
 }
 
