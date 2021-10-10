91e00b48d9f23dbf4b5dd6e6bd55b4de165e4fa0
naumtsev-ant <naumtsevanton@mail.ru>
2021-10-10 19:00:03 +0300

    Add parser

---
+++ programms/gram.tab.hpp
@@ -0,0 +1,99 @@
+/* A Bison parser, made by GNU Bison 3.5.1.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2020 Free Software Foundation,
+   Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* Undocumented macros, especially those whose name start with YY_,
+   are private implementation details.  Do not rely on them.  */
+
+#ifndef YY_YY_GRAM_TAB_HPP_INCLUDED
+# define YY_YY_GRAM_TAB_HPP_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    T_NEW = 258,
+    T_INITIALIZATION_VARIABLE = 259,
+    T_VARIABLE = 260,
+    T_STATE = 261,
+    T_EDGES_STATE_FIELD = 262,
+    T_COMMENT_STATE_FIELD = 263,
+    T_IS_TERMINAL_STATE_FIELD = 264,
+    T_EDGE = 265,
+    T_ALLOWED_LETTERS_EDGE_FIELD = 266,
+    T_BEGIN_EDGE_FIELD = 267,
+    T_END_EDGE_FIELD = 268,
+    T_BOOLEAN = 269,
+    T_STRING = 270,
+    T_OPEN_CURLY_BRACKET = 271,
+    T_CLOSE_CURLY_BRACKET = 272,
+    T_OPEN_ROUND_BRACKET = 273,
+    T_CLOSE_ROUND_BRACKET = 274,
+    T_COLON = 275,
+    T_EMPTY_LINE = 276,
+    T_SPACES = 277,
+    T_COMM = 278,
+    T_DOT = 279,
+    T_EOF = 280
+  };
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+union YYSTYPE
+{
+#line 237 "gram.ypp"
+
+    const char *str;
+
+#line 87 "gram.tab.hpp"
+
+};
+typedef union YYSTYPE YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_GRAM_TAB_HPP_INCLUDED  */
